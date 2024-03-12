01fc1f74a04dd2da796d62b67fa07b1ba4542539
Xrolllex <karolkaczanowski@wp.pl>
2024-03-13 00:05:56 +0100

    Components - maybe good maybe not
    Tutaj zrobiłem mocno szczegółowo, jakby wiadomo można to pozmieniać, wstawiłem również screeny o co mi dokładnie chodziło w niektórych komponentach jakby nie było coś jasne

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,59 @@
 import { useState } from "react";
 import "./App.css";
 
+const Header = lazy(() => import('./components/Header/Header'));
+const Login = lazy(() => import('./pages/Login/Login'));
+const Register = lazy(() => import('./pages/Registration/Register'));
+const Expenses = lazy(() => import('./pages/Home/Expenses'));
+const Income = lazy(() => import('./pages/Home/Income'));
+const ReportExpenses = lazy(() => import('./pages/Reports/RepExpenses'));
+const ReportIncome = lazy(() => import('./pages/Reports/RepIncome'));
+const NotFound = lazy(() => import('./pages/NotFound'));
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Kapusta project gr2 TEST21212</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  );
+    <Suspense fallback={<h3>Loading...</h3>}>
+      <Routes>
+      <Route path="/" element={<Header />}>
+        <Route
+              path="login"
+              element={<RestrictedRoute component={Login} path="/" />}
+            />
+        <Route
+              path="register"
+              element={<RestrictedRoute component={Register} path="/" />}
+            />
+        <Route
+            index
+            element={<PrivateRoute component={Expenses} path="/expenses" />}
+          />
+          <Route
+            index
+            element={<PrivateRoute component={Income} path="/income" />}
+          />
+
+          <Route
+            index
+            element={<PrivateRoute component={ReportExpenses} path="/reports/expenses" />}
+          />
+                 
+          <Route
+            index
+            element={<PrivateRoute component={ReportIncome} path="/reports/income" />}
+          />
+
+          
+
+          <Route path="*" element={<NotFound />} />  
+
+        </Route>
+      </Routes>
+    </Suspense>
+  </>
+ );
 }
 
 export default App;
