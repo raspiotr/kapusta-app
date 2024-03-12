a51150a6a56dbab0c0a287aff9ddb5fe06d36e16
Bartłomiej Gal <bgal23@Bartomiejs-MacBook-Pro.local>
2024-03-12 18:15:07 +0100

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">TEST TEST TEST</p>
+      <p className="read-the-docs">TEST TEST TEST TEST test</p>
     </>
   );
 }
