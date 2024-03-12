868d535cc171fcbbe580471779c32d0e0d5f2f3d
Bartłomiej Gal <bgal23@Bartomiejs-MacBook-Pro.local>
2024-03-12 18:18:14 +0100

    test branch

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">TEST TEST TEST TEST test</p>
+      <p className="read-the-docs">TEST TEST TEST TEST test test branch</p>
     </>
   );
 }
