67c110eccfb597a7c08eb20e8c92ead2f008f6d8
BGal23 <bartek.gal23@gmail.com>
2024-03-20 22:44:30 +0100

    corrected

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -1,37 +1,29 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
-import Chart from 'chart.js/auto';
+import { Bar } from "react-chartjs-2";
+import Chart from "chart.js/auto";
 import {
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-  } from 'chart.js/auto';
-
-Chart.register(
   CategoryScale,
   LinearScale,
   BarElement,
   Title,
   Tooltip,
-  Legend
-);
+  Legend,
+} from "chart.js/auto";
+
+Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const ChartComponent = ({ data }) => {
-  const labels = data.map(item => item.category);
+  const labels = data.map((item) => item.category);
   const chartData = {
     labels,
     datasets: [
       {
-        label: 'Расходы',
-        data: data.map(item => item.amount),
-        backgroundColor: 'rgba(75,192,192,0.2)',
-        borderColor: 'rgba(75,192,192,1)',
+        label: "Расходы",
+        data: data.map((item) => item.amount),
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)",
         borderWidth: 1,
-        hoverBackgroundColor: 'rgba(75,192,192,0.4)',
-        hoverBorderColor: 'rgba(75,192,192,1)',
+        hoverBackgroundColor: "rgba(75,192,192,0.4)",
+        hoverBorderColor: "rgba(75,192,192,1)",
       },
     ],
   };
--- src/pages/Reports/Reports.jsx
+++ src/pages/Reports/Reports.jsx
@@ -1,12 +1,31 @@
-import { ReportsInfo } from '../../components/Reports/ReportsInfo/ReportsInfo';
-import ReportsNav from '../../components/Reports/ReportsNav/ReportsNav'; 
-import scss from './Reports.module.scss'
- const Reports = () =>  {
+import ChartComponent from "../../components/Charts/Charts";
+import { ReportsInfo } from "../../components/Reports/ReportsInfo/ReportsInfo";
+import ReportsNav from "../../components/Reports/ReportsNav/ReportsNav";
+import scss from "./Reports.module.scss";
+
+// ----- to jest tylko testowo ----------
+const chartData = [
+  { category: "Продукты", amount: "2000" },
+  { category: "Транспорт", amount: "1500" },
+  { category: "Развлечения", amount: "1000" },
+  { category: "Продукты", amount: "2000" },
+  { category: "Транспорт", amount: "1500" },
+  { category: "Развлечения", amount: "1000" },
+  { category: "Продукты", amount: "2000" },
+  { category: "Транспорт", amount: "1500" },
+  { category: "Развлечения", amount: "1000" },
+  { category: "Продукты", amount: "2000" },
+  { category: "Транспорт", amount: "1500" },
+  { category: "Развлечения", amount: "1000" },
+];
+// ----------------------------------------
+const Reports = () => {
   return (
     <div className={scss.box}>
-      <ReportsNav/>
-      <ReportsInfo/>
+      <ReportsNav />
+      <ReportsInfo />
+      <ChartComponent data={chartData} />
     </div>
   );
 };
-export default Reports
+export default Reports;
