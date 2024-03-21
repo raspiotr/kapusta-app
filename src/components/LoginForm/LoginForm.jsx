aa014029733a0252c81f75c3a050e074dd00ba53
BGal23 <bartek.gal23@gmail.com>
2024-03-21 14:48:30 +0100

    login page corrected

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -13,42 +13,36 @@ const LoginForm = ({ onLogin }) => {
   };
 
   return (
-    <main>
-      <div className={scss.container}>
-        <form className={scss.form} onSubmit={handleSubmit}>
-          <p className={scss.firstText}>
-            You can log in with your Google Account
-          </p>
-          <div className={scss.googleLogin}>
-            <Link
-              to={
-                "https://kapusta-backend-827563b0830f.herokuapp.com/api/auth/google"
-              }
-            >
-              <img src={google} alt="" />
-              <span>Google</span>
-            </Link>
-          </div>
-          <p>Or log in using an email and password, after registering:</p>
-          <h5 className={scss.titles}>Email</h5>
-          <input
-            className={scss.inputs}
-            type="email"
-            placeholder="your@email.com"
-          />
-          <h5 className={scss.titles}>Password</h5>
-          <input
-            className={scss.inputs}
-            type="password"
-            placeholder="Password"
-          />
-          <div className={scss.buttons}>
-            <button>LOG IN</button>
-            <button>REGISTRATION</button>
-          </div>
-        </form>
-      </div>
-    </main>
+    <div>
+      <form className={scss.form} onSubmit={handleSubmit}>
+        <p className={scss.firstText}>
+          You can log in with your Google Account
+        </p>
+        <div className={scss.googleLogin}>
+          <Link
+            to={
+              "https://kapusta-backend-827563b0830f.herokuapp.com/api/auth/google"
+            }
+          >
+            <img src={google} alt="" />
+            <span>Google</span>
+          </Link>
+        </div>
+        <p>Or log in using an email and password, after registering:</p>
+        <h5 className={scss.titles}>Email:</h5>
+        <input
+          className={scss.inputs}
+          type="email"
+          placeholder="your@email.com"
+        />
+        <h5 className={scss.titles}>Password:</h5>
+        <input className={scss.inputs} type="password" placeholder="Password" />
+        <div className={scss.buttons}>
+          <button>LOG IN</button>
+          <button>REGISTRATION</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
