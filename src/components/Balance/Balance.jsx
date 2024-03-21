6507509bfd934c1c450c65a25efaa939d6073e50
BGal23 <bartek.gal23@gmail.com>
2024-03-21 20:23:55 +0100

    ask modals

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,12 +1,14 @@
-import  { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setBalance } from '../../redux/reducers/balanceReducer';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setBalance } from "../../redux/reducers/balanceReducer";
+import AsksModals from "../AsksModals/AsksModal";
 import scss from "./Balance.module.scss";
 
 export const Balance = () => {
   const dispatch = useDispatch();
-  const balanceFromRedux = useSelector(state => state.balance.balance);
+  const balanceFromRedux = useSelector((state) => state.balance.balance);
   const [inputValue, setInputValue] = useState(balanceFromRedux);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -14,30 +16,44 @@ export const Balance = () => {
 
   const confirmBalance = async () => {
     dispatch(setBalance(inputValue)); // Wysłanie akcji Reduxa z nową wartością salda
-    console.log(`Your balance: ${inputValue} UAH is confirmed`);
+    console.log(`Your balance: ${inputValue} UTH is confirmed`);
+  };
+
+  const openModalBtn = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
-    <div className={scss.container}>
-      <label className={scss.name}>Balance: </label>
-      <div className={scss.box}>
-        <div className={scss.balanceBox}>
-          <input
-            className={scss.balance}
-            id="balance"
-            type="number"
-            value={inputValue}
-            onChange={handleInputChange} // Obsługa zmiany wartości pola input
-            placeholder="00.00"
-            min="0"
-          />
-          <span className={scss.currency}>UAH</span>
-        </div>
+    <>
+      <AsksModals
+        isOpen={isOpen}
+        closeModal={openModalBtn}
+        text={"Are you sure?"}
+      />
+      <div className={scss.container}>
+        <label className={scss.name}>Balance: </label>
+        <div className={scss.box}>
+          <div className={scss.balanceBox}>
+            <input
+              className={scss.balance}
+              id="balance"
+              type="number"
+              value={inputValue}
+              onChange={handleInputChange}
+              placeholder="00.00"
+              min="0"
+            />
+            <span className={scss.currency}>UAH</span>
+          </div>
 
-        <button onClick={confirmBalance} className={scss.confirm}>
-          Confirm
-        </button>
+          <button
+            onClick={(confirmBalance, openModalBtn)}
+            className={scss.confirm}
+          >
+            Confirm
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
