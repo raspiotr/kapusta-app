d8b5989cce5b7a114ccacfe4b155089535342ad0
BGal23 <bartek.gal23@gmail.com>
2024-03-23 18:58:26 +0100

    reports fixed

--- src/pages/Reports/Reports.jsx
+++ src/pages/Reports/Reports.jsx
@@ -1,31 +1,142 @@
 import ChartComponent from "../../components/Charts/Charts";
-import { ReportsInfo } from "../../components/Reports/ReportsInfo/ReportsInfo";
-import ReportsNav from "../../components/Reports/ReportsNav/ReportsNav";
+import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+import { useEffect, useState } from "react";
 import scss from "./Reports.module.scss";
+import { getAllReportsAPI } from "../../api/apiTransaction";
+import Button from "../../components/Reports/Periods/Button/Button";
+import { Periods } from "../../components/Reports/Periods/Periods";
+import { Balance } from "../../components/Balance/Balance";
 
-// ----- to jest tylko testowo ----------
-const chartData = [
-  { category: "Продукты", amount: "2000" },
-  { category: "Транспорт", amount: "1500" },
-  { category: "Развлечения", amount: "1000" },
-  { category: "Продукты", amount: "2000" },
-  { category: "Транспорт", amount: "1500" },
-  { category: "Развлечения", amount: "1000" },
-  { category: "Продукты", amount: "2000" },
-  { category: "Транспорт", amount: "1500" },
-  { category: "Развлечения", amount: "1000" },
-  { category: "Продукты", amount: "2000" },
-  { category: "Транспорт", amount: "1500" },
-  { category: "Развлечения", amount: "1000" },
-];
-// ----------------------------------------
-const Reports = () => {
+import arrow from "../../images/SVG/arrow.svg";
+import arrowPlus from "../../images/SVG/arrow+.svg";
+import arrowMinus from "../../images/SVG/arrow-.svg";
+import { nanoid } from "@reduxjs/toolkit";
+
+const Reports = ({ balance }) => {
+  //const [amount, setAmount] = useState(0);
+  const [isActiveBtn, setIsActiveBtn] = useState([]);
+  const [isType, setIsType] = useState(true);
+  const [date, setDate] = useState(new Date());
+  const [reports, setReports] = useState();
+  const [chartData, setChartData] = useState([]);
+
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isDesktop = useMediaQuery({ minWidth: 1280 });
+
+  const balanceBox = (
+    <>
+      <div className={scss.balance}>
+        <p className={scss.title}>Balance:</p>
+        <div className={scss.amount}>
+          {balance}
+          <span>UTH</span>
+        </div>
+      </div>
+    </>
+  );
+
+  const allData = async ({ isType, year, month }) => {
+    const type = isType ? "expense" : "income";
+    try {
+      const data = await getAllReportsAPI({ type, year, month });
+      console.log(data.data);
+      return setReports(data.data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    allData({ isType, year, month });
+    setIsActiveBtn([]);
+    setChartData([]);
+  }, [isType, date]);
+
+  const checkDetail = (index, data) => {
+    setIsActiveBtn([]);
+    setChartData([...data]);
+    setIsActiveBtn((prevState) => {
+      if (prevState.includes(index)) {
+        return prevState.filter((item) => item !== index);
+      } else {
+        return [...prevState, index];
+      }
+    });
+    console.log(index, chartData);
+  };
+
   return (
-    <div className={scss.box}>
-      <ReportsNav />
-      <ReportsInfo />
-      <ChartComponent data={chartData} />
-    </div>
+    <>
+      <div className={scss.boxNav}>
+        <Link to="/">
+          <img src={arrow} alt="" />
+          {!isMobile && <span className={scss.text}>Main page</span>}
+        </Link>
+        <>
+          {isMobile ? (
+            <>
+              <Periods date={date} setDate={setDate} />
+              {balanceBox}
+            </>
+          ) : (
+            <>
+              {isDesktop ? <Balance /> : balanceBox}
+              <Periods date={date} setDate={setDate} />
+            </>
+          )}
+        </>
+      </div>
+      <div>
+        <ul className={scss.list}>
+          <li className={scss.item}>
+            <p className={scss.itemText}>Expense:</p>
+            <span className={scss.itemExpenses}>{0}.00</span>
+          </li>
+          <li className={scss.item}>
+            <p className={scss.itemText}>Income:</p>
+            <span className={scss.itemIncome}>{0}.00</span>
+          </li>
+        </ul>
+        <div className={scss.bigBox}>
+          <div className={scss.box}>
+            <Button
+              icon={arrowMinus}
+              value={isType}
+              setValue={setIsType}
+            ></Button>
+            <p className={scss.navText}>{isType ? "Expense" : "Income"}</p>
+            <Button
+              icon={arrowPlus}
+              value={isType}
+              setValue={setIsType}
+            ></Button>
+          </div>
+          <div className={scss.categoriesBox}>
+            {reports
+              ? reports.map((data, index) => (
+                  <div className={scss.category} key={nanoid()}>
+                    <span>{data.total.toFixed(2)}</span>
+                    <button
+                      className={
+                        isActiveBtn.includes(index) ? scss.activeButton : null
+                      }
+                      onClick={() => checkDetail(index, data.descriptions)}
+                    >
+                      <div></div>
+                      <img src={data.categoryImageUrl} />
+                    </button>
+                    <div>{data.category}</div>
+                  </div>
+                ))
+              : ""}
+          </div>
+        </div>
+      </div>
+      {chartData[0] && <ChartComponent data={chartData} />}
+    </>
   );
 };
 export default Reports;
