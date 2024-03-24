4b7d81707a31898f849800d9be9d2bee40ea50c0
raspiotr <raspiotr@gmail.com>
2024-03-24 15:58:26 +0100

    get token

--- src/pages/CheckUserGoogleRedir/CheckUserGoogleRedir.jsx
+++ src/pages/CheckUserGoogleRedir/CheckUserGoogleRedir.jsx
@@ -1,47 +1,54 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import { useSearchParams } from "react-router-dom";
+// import axios from "axios";
 import css from "./ChceckUserGoogleRedir.module.scss";
 
 const CheckUserGoogleRedir = () => {
-  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
+  const token = searchParams.get("token");
   useEffect(() => {
-    const checkUser = async () => {
-      // Pobierz token z nagłówka "AuthToken"
-      const token =
-        window?.location?.headers?.get("AuthToken") ||
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZmVlYWM1MzYxMjE0NDViMmRkNDY3OCIsImlhdCI6MTcxMTI3MTg0NiwiZXhwIjoxNzExODc2NjQ2fQ.dzsF1W3k-waQYQWmHmkNv4xdLAilB-gCatYGVtJak5A";
-      if (token === null) {
-        navigate("/login", { replace: true });
-      }
-      // Wyślij żądanie GET z tokenem Bearer
-      try {
-        const response = await axios.get(
-          "https://kapusta-backend-827563b0830f.herokuapp.com/api/auth/current",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
+    console.log(token);
+  }, [token]);
+  // useEffect(() => {
 
-        if (response.status === 200) {
-          const user = response.data.user;
-          console.log("Zalogowano użytkownika:", user);
-          // navigate("/", { replace: true });
-        } else {
-          //  navigate("/login", { replace: true });
-        }
-      } catch (error) {
-        console.error("Błąd:", error);
-        // navigate("/login", { replace: true });
-      }
-    };
+  //   navigate("/login", { replace: true });
+  // const checkUser = async () => {
 
-    checkUser();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  //   // Pobierz token z nagłówka "AuthToken"
+  //   const token =
+  //     window?.location?.headers?.get("AuthToken") ||
+  //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZmVlYWM1MzYxMjE0NDViMmRkNDY3OCIsImlhdCI6MTcxMTI3MTg0NiwiZXhwIjoxNzExODc2NjQ2fQ.dzsF1W3k-waQYQWmHmkNv4xdLAilB-gCatYGVtJak5A";
+  //   if (token === null) {
+  //     navigate("/login", { replace: true });
+  //   }
+  //   // Wyślij żądanie GET z tokenem Bearer
+  //   try {
+  //     const response = await axios.get(
+  //       "https://kapusta-backend-827563b0830f.herokuapp.com/api/auth/current",
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       }
+  //     );
+
+  //     if (response.status === 200) {
+  //       const user = response.data.user;
+  //       console.log("Zalogowano użytkownika:", user);
+  //       // navigate("/", { replace: true });
+  //     } else {
+  //       //  navigate("/login", { replace: true });
+  //     }
+  //   } catch (error) {
+  //     console.error("Błąd:", error);
+  //     // navigate("/login", { replace: true });
+  //   }
+  // };
+
+  // checkUser();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   return (
     <div className={css.RedirBox}>
