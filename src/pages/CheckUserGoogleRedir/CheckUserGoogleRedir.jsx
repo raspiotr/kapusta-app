fc4f6a4c633e45c59524fda4ff747ea5aefc044d
raspiotr <raspiotr@gmail.com>
2024-03-24 17:43:06 +0100

    Update CheckUserGoogleRedir.jsx

--- src/pages/CheckUserGoogleRedir/CheckUserGoogleRedir.jsx
+++ src/pages/CheckUserGoogleRedir/CheckUserGoogleRedir.jsx
@@ -1,54 +1,48 @@
 import { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
-// import axios from "axios";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 import css from "./ChceckUserGoogleRedir.module.scss";
 
 const CheckUserGoogleRedir = () => {
   const [searchParams] = useSearchParams();
 
-  const token = searchParams.get("token");
+  const navigate = useNavigate();
+
   useEffect(() => {
-    console.log(token);
-  }, [token]);
-  // useEffect(() => {
+    const token = searchParams.get("token");
+    const checkUser = async () => {
+      // Pobierz token z URL
 
-  //   navigate("/login", { replace: true });
-  // const checkUser = async () => {
+      if (token === null) {
+        navigate("/login", { replace: true });
+      }
+      // Wyślij żądanie GET z tokenem Bearer
+      try {
+        const response = await axios.get(
+          "https://kapusta-backend-827563b0830f.herokuapp.com/api/auth/current",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
 
-  //   // Pobierz token z nagłówka "AuthToken"
-  //   const token =
-  //     window?.location?.headers?.get("AuthToken") ||
-  //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZmVlYWM1MzYxMjE0NDViMmRkNDY3OCIsImlhdCI6MTcxMTI3MTg0NiwiZXhwIjoxNzExODc2NjQ2fQ.dzsF1W3k-waQYQWmHmkNv4xdLAilB-gCatYGVtJak5A";
-  //   if (token === null) {
-  //     navigate("/login", { replace: true });
-  //   }
-  //   // Wyślij żądanie GET z tokenem Bearer
-  //   try {
-  //     const response = await axios.get(
-  //       "https://kapusta-backend-827563b0830f.herokuapp.com/api/auth/current",
-  //       {
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //       }
-  //     );
+        if (response.status === 200) {
+          const name = response.data.user.name;
+          console.log("Zalogowano użytkownika:", name);
+          navigate("/", { replace: true });
+        } else {
+          navigate("/login", { replace: true });
+        }
+      } catch (error) {
+        console.error("Błąd:", error);
+        navigate("/login", { replace: true });
+      }
+    };
 
-  //     if (response.status === 200) {
-  //       const user = response.data.user;
-  //       console.log("Zalogowano użytkownika:", user);
-  //       // navigate("/", { replace: true });
-  //     } else {
-  //       //  navigate("/login", { replace: true });
-  //     }
-  //   } catch (error) {
-  //     console.error("Błąd:", error);
-  //     // navigate("/login", { replace: true });
-  //   }
-  // };
-
-  // checkUser();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
+    checkUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className={css.RedirBox}>
