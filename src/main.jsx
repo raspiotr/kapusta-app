188f2e7f423987f1420ae029ca11134e523b6fcd
raspiotr <raspiotr@gmail.com>
2024-03-25 11:40:42 +0100

    change routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
@@ -13,9 +12,7 @@ createRoot(document.getElementById("root")).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/goit-gr2-kapusta">
-          <Suspense fallback={<h3>Loading...</h3>}>
-            <App />
-          </Suspense>
+          <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
