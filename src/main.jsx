8c8b73acbf65e377a3c824308f10274d4ec07dd5
raspiotr <raspiotr@gmail.com>
2024-03-28 22:30:23 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Suspense } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { store, persistor } from "../src/redux/store.js";
@@ -12,11 +12,11 @@ createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <Suspense fallback={<h3>Loading...</h3>}>
-            <App />
-          </Suspense>
-        </BrowserRouter>
+        {/* <BrowserRouter> */}
+        <Suspense fallback={<h3>Loading...</h3>}>
+          <App />
+        </Suspense>
+        {/* </BrowserRouter> */}
       </PersistGate>
     </Provider>
   </React.StrictMode>
