c5009af0106e2a9d453c833cfd77b3edb5eb172c
raspiotr <raspiotr@gmail.com>
2024-03-28 15:59:32 +0100

    change connections

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/goit-gr2-kapusta",
+  base: "/kapusta-app",
   plugins: [react()],
 });
