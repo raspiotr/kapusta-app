6b00ab0876364ffde61d97f35f0a999ecc766749
raspiotr <raspiotr@gmail.com>
2024-03-28 21:56:46 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/kapusta-app">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
